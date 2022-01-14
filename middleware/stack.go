af9da692587175c01974b981ee933d39afd12541
Nadir Miralimov <riid@protonmail.com>
2022-01-14 19:53:47 +0200

    Revert "prevent context leak in stack middleware"
    This reverts commit a1801e60f6835dd59030404f6084008e0f7fc25c.

--- middleware/stack.go
+++ middleware/stack.go
@@ -15,16 +15,13 @@ type stack struct {
 }
 
 func (s *stack) Handle(ctx context.Context, b messenger.Dispatcher, e messenger.Envelope, next messenger.NextFunc) {
-
-	inStackNext := func(ctx context.Context, lastE messenger.Envelope) { e = lastE }
 	for i := len(s.middlewares) - 1; i >= 0; i-- {
-		inStackNext = func(m messenger.Middleware, next messenger.NextFunc) messenger.NextFunc {
+		next = func(m messenger.Middleware, next messenger.NextFunc) messenger.NextFunc {
 			return func(ctx context.Context, e messenger.Envelope) {
 				m.Handle(ctx, b, e, next)
 			}
-		}(s.middlewares[i], inStackNext)
+		}(s.middlewares[i], next)
 	}
 
-	inStackNext(ctx, e)
 	next(ctx, e)
 }
