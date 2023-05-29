9f56bb370a56781d82242e508e37bbb8589d0262
tomcyr <tomek@concept-it.pl>
2023-05-29 11:39:03 +0200

    use uint wrapper for priority

--- envelope/priority.go
+++ envelope/priority.go
@@ -1,20 +1,24 @@
 package envelope
 
-import "github.com/riid/messenger"
+import (
+	"github.com/riid/messenger"
+)
 
 const priorityHeader = "X-Message-Priority"
 
-func WithPriority(wrapped messenger.Envelope, priority string) messenger.Envelope {
-	return WithHeader(wrapped, priorityHeader, priority)
+func WithPriority(wrapped messenger.Envelope, priority uint) messenger.Envelope {
+	return WithUint(wrapped, priorityHeader, priority)
 }
 
 func WithoutPriority(e messenger.Envelope) messenger.Envelope {
 	return WithoutHeader(e, priorityHeader)
 }
 
-func Priority(e messenger.Envelope) string {
-	if ct, found := e.LastHeader(priorityHeader); found {
-		return ct
+func Priority(e messenger.Envelope) uint {
+	v, err := Uint(e, priorityHeader)
+	if err != nil {
+		return 0
 	}
-	return ""
+
+	return v
 }
--- envelope/priority_test.go
+++ envelope/priority_test.go
@@ -7,16 +7,16 @@ import (
 )
 
 func TestWithPriority(t *testing.T) {
-	e := WithPriority(WithPriority(FromMessage(nil), "1"), "2")
-	assert.Equal(t, "2", Priority(e))
+	e := WithPriority(WithPriority(FromMessage(nil), 1), 2)
+	assert.Equal(t, uint(2), Priority(e))
 }
 
 func TestWithoutPriority(t *testing.T) {
-	e := WithoutPriority(WithPriority(FromMessage(nil), "1"))
-	assert.Equal(t, "", Priority(e))
+	e := WithoutPriority(WithPriority(FromMessage(nil), 1))
+	assert.Equal(t, uint(0), Priority(e))
 }
 
 func TestPriority_when_empty(t *testing.T) {
 	e := FromMessage(nil)
-	assert.Equal(t, "", Priority(e))
+	assert.Equal(t, uint(0), Priority(e))
 }
